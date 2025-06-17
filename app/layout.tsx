b06a519744486372cc08670f304194e0d79081ac
Nawod <dev.nawod@gmail.com>
2025-06-17 19:47:26 +0530

    add google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -106,6 +106,17 @@ export default function RootLayout({
 					content="ca-pub-2660491671296139"
 				/>
 				<Script
+					id="gtm-script"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-WND3FCHX');`,
+					}}
+				/>
+				<Script
 					async
 					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2660491671296139"
 					crossOrigin="anonymous"
